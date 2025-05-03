f06c1101a71a98d0f726394af6265f48ab49cb8c
Eric Seifer <eric.seifer@web.de>
2025-05-03 22:54:28 +0000

    Bsp main Angepasst

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,7 @@
 
 #include "pico/stdlib.h"
 #include "pico/time.h"
-
+#include "hardware/timer.h"
 
 #include <string.h>
 #include <math.h>
@@ -16,20 +16,41 @@
 
 #define NET_DNS 1
 
+bool lock_timer = false; 
+HomeAssistant_MQTT* mqtt_client_public = NULL;
+
+void gpio_callback(uint gpio, uint32_t events) {
+    if(gpio_get(gpio) == 1) {
+        mqtt_client_public->publish("HomeAutomation/lock/Haustuer/state", "UNLOCKED");
+        return;
+    };
+    mqtt_client_public->publish("HomeAutomation/lock/Haustuer/state", "LOCKED");
+};
+
+int64_t abschliessen(alarm_id_t id, void *arg) {
+    lock_timer = false;
+    HomeAssistant_MQTT* mqtt_client = (HomeAssistant_MQTT*)arg;
+    gpio_put(15, 0);
+    return 0;
+}
+
 
 void lock_callback(void* arg, char* payload, size_t len) {
     HomeAssistant_MQTT* mqtt_client = (HomeAssistant_MQTT*)arg;
     printf("Lock command received: %.*s\n", (int)len, payload);
     if (strncmp(payload, "LOCK", len) == 0) {
+
         printf("Locking the door...\n");
-        mqtt_client->publish("HomeAutomation/lock/Haustuer/state", "LOCKED");
-        gpio_put(15, 1);
+        gpio_put(15, 0);
         
         // Add your lock logic here
     } else if (strncmp(payload, "UNLOCK", len) == 0) {
+        if (!lock_timer) {
+            lock_timer = true;
+            alarm_id_t id = add_alarm_in_ms(3000, abschliessen, arg, true);
+        }
         printf("Unlocking the door...\n");
-        mqtt_client->publish("HomeAutomation/lock/Haustuer/state", "UNLOCKED");
-        gpio_put(15, 0);
+        gpio_put(15, 1);
         // Add your unlock logic here
     } else {
         printf("Unknown command: %.*s\n", (int)len, payload);
@@ -43,7 +64,11 @@ int main(){
     printf("\nHallo welt\n\n");
     gpio_init(15);
     gpio_set_dir(15, GPIO_OUT);
-    
+    gpio_pull_down(15);
+    gpio_init(14);
+    gpio_set_dir(14, GPIO_IN);
+    gpio_pull_down(14);
+
     
     if(!connect_wifi())
         return -1;
@@ -55,7 +80,10 @@ int main(){
     mqtt_client.connect();
     mqtt_client.registerHandler("HomeAutomation/lock/Haustuer/command", lock_callback, (void*)&mqtt_client);
     mqtt_client.publish(discovery_topic, entity_type);
-
+    mqtt_client_public = &mqtt_client;
+    gpio_put(15, 0);
+    mqtt_client.publish("HomeAutomation/lock/Haustuer/state", "LOCKED");
+    gpio_set_irq_enabled_with_callback(14, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     while(true) {
         sleep_ms(100);
